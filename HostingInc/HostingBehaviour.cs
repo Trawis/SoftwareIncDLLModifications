b381d7131203f10e6df2c33a710da49b7c8a771b
Trawis <kichoky@gmail.com>
2017-05-23 23:24:22 +0200

    New Project

---
+++ HostingInc/HostingBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HostingInc
+{
+    public class HostingBehaviour : ModBehaviour
+    {
+        void Start()
+        { }
+        void Update()
+        { }
+        public override void OnActivate()
+        { }
+        public override void OnDeactivate()
+        { }
+    }
+    public static class Extensions
+    {
+        /// <summary>
+        /// Sets a _private_ Property Value from a given Object. Uses Reflection.
+        /// Throws a ArgumentOutOfRangeException if the Property is not found.
+        /// </summary>
+        /// <typeparam name="T">Type of the Property</typeparam>
+        /// <param name="obj">Object from where the Property Value is set</param>
+        /// <param name="propName">Propertyname as string.</param>
+        /// <param name="val">Value to set.</param>
+        /// <returns>PropertyValue</returns>
+        public static void SetPrivatePropertyValue<T>(this object obj, string propName, T val)
+        {
+            Type t = obj.GetType();
+            t.InvokeMember(propName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.SetProperty | BindingFlags.Instance, null, obj, new object[] { val });
+        }
+    }
+}
---
+++ HostingInc/Main.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Net.Mime;
+using System.Collections.Generic;
+
+namespace HostingInc
+{
+    public class Main : ModMeta
+    {
+        public void ConstructOptionsScreen(RectTransform parent, ModBehaviour[] behaviours)
+        {
+
+        }
+        public string Name
+        {
+            //This will be displayed as the header in the Options window
+            get { return "Hosting Inc"; }
+        }
+    }
+}
